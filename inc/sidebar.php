90c0dbd339eac3ba67aa59916ef819f5c8ed6b5c
unknown <yemrekaya99@gmail.com>
2021-05-04 23:50:54 +0300

    Sidebar

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -6,7 +6,16 @@
                             </header>
                             <div class="filter-content">
                                 <div class="list-group list-group-flush">
-                                <?php ?>
+                                <?php  
+                                $sql = "SELECT * FROM categories";
+                                $select_all_categories = mysqli_query($conn, $sql);
+                                
+                                while($row = mysqli_fetch_assoc($select_all_categories))
+                                {
+                                    $category_name = $row["c_name"];                                
+                                    echo "<a href='#' class='list-group-item'>{$category_name}<span class='float-end badge badge-light round bg-k20secondary'>X</span> </a>"; 
+                                }
+                                ?>
                                 <!--<a href="#" class="list-group-item">Kategori 1<span
                                             class="float-end badge badge-light round bg-k20secondary">12</span> </a>
                                     <a href="#" class="list-group-item">Kategori 2<span
